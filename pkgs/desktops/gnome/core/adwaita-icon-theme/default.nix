6ce98e5f3b35ced122882c5bcfa7d8815c37b463
Maxine Aubrey <max@ine.dev>
2024-03-18 20:22:06 +0100

    gnome.adwaita-icon-theme: 46.rc → 46.0
    https://gitlab.gnome.org/GNOME/adwaita-icon-theme/-/compare/46.rc...46.0

--- pkgs/desktops/gnome/core/adwaita-icon-theme/default.nix
+++ pkgs/desktops/gnome/core/adwaita-icon-theme/default.nix
@@ -13,11 +13,11 @@
 
 stdenv.mkDerivation rec {
   pname = "adwaita-icon-theme";
-  version = "46.rc";
+  version = "46.0";
 
   src = fetchurl {
     url = "mirror://gnome/sources/adwaita-icon-theme/${lib.versions.major version}/${pname}-${version}.tar.xz";
-    sha256 = "sha256-R0XjL6bX2UX5TShpvI4gAtpHzNWabVFww6n2MYnTv6Q=";
+    sha256 = "sha256-S8tTm9ddZNo4XW+gjLqp3erOtqyOgrhbpsQRF79bpk4=";
   };
 
   nativeBuildInputs = [
