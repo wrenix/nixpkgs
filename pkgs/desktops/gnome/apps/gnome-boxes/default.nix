d8bf65185aef6cfa4fe4fd4c2eda63e603ee6101
Maxine Aubrey <max@ine.dev>
2024-03-19 22:50:04 +0100

    gnome.gnome-boxes: 45.0 → 46.0
    https://gitlab.gnome.org/GNOME/gnome-boxes/-/compare/45.0...46.0

--- pkgs/desktops/gnome/apps/gnome-boxes/default.nix
+++ pkgs/desktops/gnome/apps/gnome-boxes/default.nix
@@ -48,11 +48,11 @@
 
 stdenv.mkDerivation rec {
   pname = "gnome-boxes";
-  version = "45.0";
+  version = "46.0";
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${lib.versions.major version}/${pname}-${version}.tar.xz";
-    sha256 = "zGMIDu+hR6hHKrGl/wh7l6J6tyOk7gBe1B6Mndd5jkE=";
+    sha256 = "Z+616YkpSViyXDl8/FYV/2X0QxXHAAEPC54zbm80B8s=";
   };
 
   patches = [
